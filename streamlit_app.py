a2fdb3118c43f1de25e7f28e56cd88da097291af
sohanjammula <jammulasohan115@gmail.com>
2025-04-22 17:37:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,12 @@ st.write("You selected:", option)
 conn = st.connection("snowflake")
 Session = conn.session()
 my_dataframe = Session.table("smoothies.public.fruit_options").select(col('fruit_name'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+
+pd_df = my_dataframe.to_pandas()
+#st.dataframe(pd_df)
+#st.stop()
+
 ingredients_list = st.multiselect('choose upto four ingredients:'
                                  , my_dataframe
                                   ,max_selections=5
@@ -39,6 +44,9 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen + ''
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+      
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
         sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width = True)
